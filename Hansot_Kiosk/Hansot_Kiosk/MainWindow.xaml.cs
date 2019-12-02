bf04ad06cf479e83fc7033731bf8f3ed411a8b26
Seongheon <dev.seongheon@gmail.com>
2019-12-02 22:31:08 +0900

    연습
    메인, 주문화면 조금

--- Hansot_Kiosk/Hansot_Kiosk/MainWindow.xaml.cs
+++ Hansot_Kiosk/Hansot_Kiosk/MainWindow.xaml.cs
@@ -20,9 +20,37 @@ namespace Hansot_Kiosk
     /// </summary>
     public partial class MainWindow : Window
     {
+        string[] images = new string[4];
+        int imagesIdx = 0;
         public MainWindow()
         {
             InitializeComponent();
+
+            images[0] = "C:\\Users\\user\\Desktop\\한솥\\한솥이벤트1.png";
+            images[1] = "C:\\Users\\user\\Desktop\\한솥\\한솥이벤트2.jpg";
+            images[2] = "C:\\Users\\user\\Desktop\\한솥\\한솥이벤트3.jpg";
+            images[3] = "C:\\Users\\user\\Desktop\\한솥\\한솥이벤트4.jpg";
+            eventImg.Source = new BitmapImage(new Uri(images[0]));
         }
-    }
+
+        private void nextBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (++imagesIdx == 4)
+                imagesIdx = 0;
+            eventImg.Source = new BitmapImage(new Uri(images[imagesIdx]));
+        }
+
+        private void beforeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (--imagesIdx == -1)
+                imagesIdx = 3;
+            eventImg.Source = new BitmapImage(new Uri(images[imagesIdx]));
+        }
+
+        private void orderBtn_Click(object sender, RoutedEventArgs e)
+        {
+            new Order().Show();
+            this.Close();
+        }
+    }   
 }
---
+++ Hansot_Kiosk/Hansot_Kiosk/Order.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Hansot_Kiosk
+{
+    /// <summary>
+    /// Order.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class Order : Window
+    {
+        public Order()
+        {
+            InitializeComponent();
+        }
+
+        private void endBtn_Click(object sender, RoutedEventArgs e)
+        {
+            new MainWindow().Show();
+            this.Close();
+        }
+    }
+}
